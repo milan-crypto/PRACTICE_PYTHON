0ba77fac8f4df5e0d71a9f87848165cafd15497a
milan-crypto <85555923+milan-crypto@users.noreply.github.com>
2021-07-07 15:17:46 +0100

    complete and functional

--- fibonnaci.py
+++ fibonnaci.py
@@ -5,9 +5,33 @@
 # 13, …)
 
 
-i= int(input("Please select a number: ")) # the number you want to work out.
-n=0
-while i<=1000000: # range to 1 million
-    i = i + n # formula
-    n = i - n # formula
-    print(i) # print command
+# i = int(input("Please select a number: "))
+# n = 0
+# while i <= 1000000:
+#     i = i + n
+#     n = i - n
+#     print(i)
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
